aaf1ecd12d0ff1a92b175057ebecf53f3852c797
Jesús <noquedanadalibre@gmail.com>
2015-08-20 17:23:10 +0200

    function savescroll

--- js/readingBar.js
+++ js/readingBar.js
@@ -1,11 +1,10 @@
 /*
 TODO LIST
 
-- apply to article
-- config position
-- on resize
-- font relative - height
-- marker localstorage
+- apply only to article.
+- config position.
+- font relative - height.
+- marker localstorage.
 */
 
 (function($) {
@@ -13,7 +12,6 @@ TODO LIST
 		readingbar: function(options) {
 
 			/* printing readingBar */
-	
 			var bar = document.createElement("div");
 			bar.className = "bar";
 			document.body.appendChild(bar);
@@ -24,45 +22,46 @@ TODO LIST
 			time.className = "time";
 			bar.appendChild(time);
 
+			var saveScroll = document.createElement("div");
+			saveScroll.className = "saveScroll";
+			bar.appendChild(saveScroll);
+
 			var mycontent = $("#my-content").html();
 			var splitwords = mycontent.split(/\b[\s,\.\-:;]*/);
 			var total = splitwords.length;
 
+			/* estimate time */
 			var estimateTime = (total * 60) / 180;
 			var estimateTimeMinutes = Math.round(estimateTime / 60);
 			$(".time").html("Read in " + estimateTimeMinutes + " min.");
 
+			$(".saveScroll").html("go to last position");
 
-			var totalScroll = $(document).height() - $(window).height();
+			var totalScroll = $(document).height() - $("#my-content").height();
 			var offsetArt = $("#my-content").offset().top;
 			var offsetPercent = Math.round((offsetArt * 100) / totalScroll); 
-
 
 			var percent = ($(document).scrollTop() * 100) / totalScroll;
 			var finalPercent = Math.round(percent) + "%";
 			$(".complete").html("<span class='percent'>" + finalPercent + "</span>");
 			$(".complete").css("width", finalPercent);
 
-			/* Options */
-
 			settings();
+			//saveScroll();
+
 
 			$(window).scroll(function(){
-				var totalScroll = $(document).height() - $(window).height();
+				//var totalScroll = $(document).height() - $(window).height();
+				var totalScroll = $(document).height() - ($("#my-content").position().top + $(window).height());
 				percent = ($(document).scrollTop() * 100) / totalScroll;
 				finalPercent = Math.round(percent) + "%";
-
 				$(".complete").css("width", finalPercent);
-
 				settings();
-
 			});
-
 
 			function settings(){
 
 				if(options){
-
 					if(options.percent == false) {
 						$(".complete").html("<span class='percent'>" + " " + "</span>");
 					} else {
@@ -74,11 +73,20 @@ TODO LIST
 					}
 					if(options.height != "") {
 						$(".bar").css("height", options.height + "px");	
-
 					}
 				}
-
 			}
+
+			function saveScroll() {
+				var scrollpos = localStorage.getItem('scrollpos');
+				window.scrollTo(0, scrollpos);
+
+				window.onscroll = function(evt) {
+					scrollpos = evt.pageY;
+					localStorage.setItem('scrollpos', scrollpos);
+				}
+			}
+
 		}
 	})
 })(jQuery)
